06e4eea8aba1b6a963ba8f14aabff0e7e110e0b8
Pavel Bezdornov <pavel.alpinweb@yandex.ru>
2018-07-27 17:35:36 +0600

    complete task 1-5

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {
+function returnFirstArgument(parametr) {
+    return parametr;
 }
 
 /*
@@ -30,6 +31,11 @@ function returnFirstArgument() {
    sumWithDefaults(10) вернет 110
  */
 function sumWithDefaults(a, b) {
+    if(b === undefined){
+        b = 100;
+    }
+    var sum = a + b;
+    return sum;
 }
 
 /*
@@ -41,6 +47,7 @@ function sumWithDefaults(a, b) {
    returnFnResult(() => 'привет') вернет 'привет'
  */
 function returnFnResult(fn) {
+    return fn();
 }
 
 /*
@@ -57,6 +64,13 @@ function returnFnResult(fn) {
    console.log(f()); // выведет 13
  */
 function returnCounter(number) {
+    if(number === undefined){
+        number = 0;
+    }
+    return function(){
+       number++
+       return number;
+    }
 }
 
 /*
@@ -69,6 +83,7 @@ function returnCounter(number) {
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
 function returnArgumentsArray() {
+    return Array.from(arguments);
 }
 
 /*
@@ -87,6 +102,12 @@ function returnArgumentsArray() {
    console.log(newSum()) выведет 6
  */
 function bindFunction(fn) {
+    var myFunc = fn();
+    for (let i = 1; i < arguments.length; i++) {
+        myFunc.bind(arguments[i]);    
+    }
+
+    return myFunc;
 }
 
 export {
