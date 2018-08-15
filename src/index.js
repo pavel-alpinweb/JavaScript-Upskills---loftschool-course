6577cd42ab7732719f30085ce7547449c0e85af0
Pavel Bezdornov <pavel.alpinweb@yandex.ru>
2018-08-15 21:04:25 +0600

    home task complete without star

---
+++ src/index.js
@@ -0,0 +1,62 @@
+/* ДЗ 6 - Асинхронность и работа с сетью */
+
+/*
+ Задание 1:
+
+ Функция должна возвращать Promise, который должен быть разрешен через указанное количество секунду
+
+ Пример:
+   delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
+ */
+function delayPromise(seconds) {
+    // Создаётся объект promise
+    let promise = new Promise((resolve, reject) => {
+
+        setTimeout(() => resolve(), seconds*1000);
+    
+    });
+
+    return promise;
+}
+
+/*
+ Задание 2:
+
+ 2.1: Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+
+ Массив городов пожно получить отправив асинхронный запрос по адресу
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+
+ 2.2: Элементы полученного массива должны быть отсортированы по имени города
+
+ Пример:
+   loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
+ */
+function loadAndSortTowns() {
+
+    let promise = new Promise((resolve, reject) => {
+        var req = new XMLHttpRequest();
+        req.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json', true);
+        req.responseType = "json";
+        req.onload = function() {
+            let myJson  = req.response;
+            myJson.sort(function(a, b){
+                if(a.name < b.name) return -1;
+                if(a.name > b.name) return 1;
+                return 0;
+            });
+            resolve(myJson);
+        };
+        req.send();
+    });
+
+    return promise;
+    
+}
+
+loadAndSortTowns().then(towns => console.log(towns));
+
+export {
+    delayPromise,
+    loadAndSortTowns
+};
